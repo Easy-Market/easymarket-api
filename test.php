0f982c8c67ed08e5ecec67f4280d6117493af66c
Precious Tom <tomprezine@gmail.com>
2020-01-24 16:02:41 +0100

    check for email exit onjoin

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+	/**
+	 * You can test using this URL and Parameters (Change where appropriate )
+	 * http://127.0.0.1/easymarket-api/app/model/join?
+	 * email=tomprezine@gmail.com&
+	 * phone=08179685649&
+	 * password=root&
+	 * first_name=Precious&
+	 * last_name=Tom&
+	 * business_name=Circlepanda&
+	 * cansell=1&
+	 * displaymode=1&
+	 * url=circlepanda
+	 */
